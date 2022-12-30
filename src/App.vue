4c5454ad18a019b3b71403e71ab6e8dd56363dd7
Oliver Burgmaier <oliver.burgmaier@gmail.com>
2022-12-30 18:07:06 +0100

    App simplified

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <router-view />
+  <v-app>
+    <v-main>
+      <router-view />
+      <SnackbarCmp />
+      <ReloadPrompt />
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
 import { useTheme } from 'vuetify'
+import SnackbarCmp from '@/components/SnackbarCmp.vue'
+import ReloadPrompt from '@/components/ReloadPrompt.vue';
+
 // get the os theme
 let themeSetting = 'light'
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: () => import('@/App.vue'),
     children: [
       {
         path: '',
